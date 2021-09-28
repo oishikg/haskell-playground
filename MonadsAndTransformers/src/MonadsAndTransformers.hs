faec4de6c59d5b0fa6ffbb949e1a852454332720
Oishik Ganguly <oishikganguly74@gmail.com>
2021-09-29 03:14:02 +0800

    Nesting functors and applicatives + MaybeT

--- MonadsAndTransformers/src/MonadsAndTransformers.hs
+++ MonadsAndTransformers/src/MonadsAndTransformers.hs
@@ -1013,15 +1013,34 @@ putThoseReaderComputationsTogether stockPrice = do
 runReaderComputation :: SomeEnv -> MyReader SomeEnv (Int, Int) -> (Int, Int)
 runReaderComputation env action = runMyReader action env
 
-
-
-
-
-
 {- | 9. Nesting functors and applicatives -}
+
+newtype Compose f g a = Compose { runCompose :: f (g a) }
+
+composetTestVal1 :: Compose [] Maybe Integer
+composetTestVal1 = Compose [Just 1, Just 2, Just 3]
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+    fmap :: (a -> b) -> Compose f g a -> Compose f g b
+    fmap f (Compose fga) = Compose $ fmap (fmap f) fga -- functors are closed under composition
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+    pure :: a -> Compose f g a
+    pure a = Compose $ pure $ pure a
+
+    (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+    (<*>) (Compose fgab) (Compose fga) = Compose $ fmap (<*>) fgab <*> fga
+    -- Reasoning:
+    -- :t (fmap (<*>) fgab) = f (g a -> g b)
+    -- Now apply a value of this type to a value of type f (g a) to get f ( g b )
 
 {- | 10. Nesting monads? A motivation for monad transformers -}
 
-{- | 11. ReaderT -}
+-- Unfortunately, this cannot be done for monads
 
-{- | 12. State and StateT -}
+{- | 11. MaybeT -}
+
+newtype MyMaybeT m a = MyMaybeT { runMyMaybeT :: m (Maybe a) }
+
+mmtTestVal1 :: MyMaybeT Maybe Integer
+mmtTestVal1 = MyMaybeT $ Just (Just 3)
